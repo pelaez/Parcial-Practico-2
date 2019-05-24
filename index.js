04f606fe46ac6e0b6daf63166b110fda926db9f8
Juan Carlos Pelaez <jucape7@gmail.com>
2019-05-23 19:01:43 -0500

    Se instalaron las dependencias de express y express-handlebars

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require('express');
 
+//importa el modulo express
+const express = require('express');
+const motorRender = require('express-handlebars');
+
+//crear la variable app que use express
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars',motorRender());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+    var contexto = {
+      titulo: 'Inicio',
+    };
+    res.render('home', contexto);
+    //res.sendFile(__dirname + '/public/index.html');
+  });
 
 app.listen(3000, function(){
     console.log('escuchando el puerto 3000');
